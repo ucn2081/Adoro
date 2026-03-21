ba6798b9d961b3b1271357b81021a2606aeb3725
ucn2081 <140593816+ucn2081@users.noreply.github.com>
2026-03-21 17:11:43 +0100

    Add files via upload

--- comfyui-dl.sh
+++ comfyui-dl.sh
@@ -9,6 +9,16 @@ wget  -O /workspace/ComfyUI/custom_nodes/ComfyUi-ImageGalleryLoader/custom_sourc
 DL="${COMFYUI_DIR}/models/loras"
 # cd "$path" 
 
+
+wget -nc -O "${DL}/reswapper_128.onnx" "https://huggingface.co/datasets/Gourieff/ReActor/resolve/main/models/reswapper_128.onnx"
+wget -nc -O "${DL}/reswapper_256.onnx" "https://huggingface.co/datasets/Gourieff/ReActor/resolve/main/models/reswapper_256.onnx"
+
+wget -nc -O "/workspace/ComfyUI/models/checkpoints/real-world_ccsr-fp16.safetensors" https://huggingface.co/Kijai/ccsr-safetensors/resolve/main/real-world_ccsr-fp16.safetensors
+
+wget -nc -O /workspace/ComfyUI/models/diffusion_models/wan2.2-i2v-rapid-aio-v10-nsfw-Q6_K.gguf https://huggingface.co/befox/WAN2.2-14B-Rapid-AllInOne-GGUF/resolve/main/v10/wan2.2-i2v-rapid-aio-v10-nsfw-Q6_K.gguf
+wget -nc -O /workspace/ComfyUI/models/diffusion_models/wan2.2-rapid-mega-aio-nsfw-v12.1-Q6_K.gguf   https://huggingface.co/befox/WAN2.2-14B-Rapid-AllInOne-GGUF/resolve/main/Mega-v12/wan2.2-rapid-mega-aio-nsfw-v12.1-Q6_K.gguf
+wget -nc -O /workspace/ComfyUI/models/diffusion_models/Phantom_Wan_14B_FusionX-Q6_K.gguf https://huggingface.co/QuantStack/Phantom_Wan_14B_FusionX-GGUF/resolve/main/Phantom_Wan_14B_FusionX-Q6_K.gguf
+
 wget -nc -O "${DL}/Bouncy SqueezyV1.safetensors"   "https://civitai.com/api/download/models/2345430?token=9ffbafa226d031653b7ea196c6334fe0"
 wget -nc -O "${DL}/fingering_i2v_e248.safetensors" "https://civitai.com/api/download/models/1694292?token=9ffbafa226d031653b7ea196c6334fe0"
 wget -nc -O "${DL}/better pussy than ... Wan480p v1.0.safetensors" "https://civitai.com/api/download/models/2056986?token=9ffbafa226d031653b7ea196c6334fe0"
@@ -31,15 +41,6 @@ wget -nc -O "${DL}/wan22_i2v_taker_pov_bj_low_noise.safetensors" "https://civita
 wget -nc -O "${DL}/wan-i2v-futa-trans-cowgirl-v1.safetensors"   "https://civitai.com/api/download/models/1553795?token=9ffbafa226d031653b7ea196c6334fe0"
 wget -nc -O "${DL}/KZ.safetensors" "https://civitai.com/api/download/models/2157825?token=9ffbafa226d031653b7ea196c6334fe0"
 wget -nc -O "${DL}/lightx2v_T2V_14B_cfg_step_distill_v2_lora_rank32_bf16.safetensors" "https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Lightx2v/lightx2v_T2V_14B_cfg_step_distill_v2_lora_rank32_bf16.safetensors"
-
-wget -nc -O "${DL}/reswapper_128.onnx" "https://huggingface.co/datasets/Gourieff/ReActor/resolve/main/models/reswapper_128.onnx"
-wget -nc -O "${DL}/reswapper_256.onnx" "https://huggingface.co/datasets/Gourieff/ReActor/resolve/main/models/reswapper_256.onnx"
-
-wget -nc -O "/workspace/ComfyUI/models/checkpoints/real-world_ccsr-fp16.safetensors" https://huggingface.co/Kijai/ccsr-safetensors/resolve/main/real-world_ccsr-fp16.safetensors
-
-wget -nc -O /workspace/ComfyUI/models/diffusion_models/wan2.2-i2v-rapid-aio-v10-nsfw-Q6_K.gguf https://huggingface.co/befox/WAN2.2-14B-Rapid-AllInOne-GGUF/resolve/main/v10/wan2.2-i2v-rapid-aio-v10-nsfw-Q6_K.gguf
-wget -nc -O /workspace/ComfyUI/models/diffusion_models/wan2.2-rapid-mega-aio-nsfw-v12.1-Q6_K.gguf   https://huggingface.co/befox/WAN2.2-14B-Rapid-AllInOne-GGUF/resolve/main/Mega-v12/wan2.2-rapid-mega-aio-nsfw-v12.1-Q6_K.gguf
-wget -nc -O /workspace/ComfyUI/models/diffusion_models/Phantom_Wan_14B_FusionX-Q6_K.gguf https://huggingface.co/QuantStack/Phantom_Wan_14B_FusionX-GGUF/resolve/main/Phantom_Wan_14B_FusionX-Q6_K.gguf
 
 
 # wget -nc -O "/workspace/ComfyUI/models/checkpoints/illustrij_v21.safetensors"  "https://civitai.com/api/download/models/2728617?token=9ffbafa226d031653b7ea196c6334fe0"
